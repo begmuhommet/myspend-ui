fc397166933024fe06f4d831400e39e7d3e6fa5f
bega <begmuhommet.jumayev@gmail.com>
2024-12-28 14:01:14 +0800

    added header and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import { App, Block, Button, Navbar, Page } from 'konsta/react';
+import { App, Page } from 'konsta/react';
+import { useState } from 'react';
+import Header from './components/header';
+import Navbar from './components/navbar';
+import AppRouter from './router/AppRouter';
 
 function MyApp() {
+  // States
+  const [options, setOptions] = useState({ platform: 'ios', theme: 'dark' });
+
+  // Handlers
+  const handleChangeTheme = (value: string) => {
+    setOptions((prevState) => ({ ...prevState, theme: value }));
+  };
+
+  const handleChangePlatform = (value: string) => {
+    setOptions((prevState) => ({ ...prevState, platform: value }));
+  };
+
+  // Renders
   return (
-    <App safeAreas theme="ios">
+    <App safeAreas theme={options.platform} dark={options.theme === 'dark'}>
       <Page>
-        <Navbar title="My App" />
-        <Block>
-          <p>Here comes my app</p>
-        </Block>
-        <Button color="orange">Click me</Button>
+        <Header
+          platform={options.platform}
+          theme={options.theme}
+          onChangePlatform={handleChangePlatform}
+          onChangeTheme={handleChangeTheme}
+        />
+        <AppRouter />
+        <Navbar />
       </Page>
     </App>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,6 @@ import konstaConfig from 'konsta/config';
 
 const tailwindConfig = konstaConfig({
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  konsta: {
-    colors: {
-      primary: '#F87315',
-    },
-  },
   theme: {
     extend: {},
   },
