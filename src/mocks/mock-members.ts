03673e1eb320fe8c6894eb15f1d5da5e0ce1d1fd
bega <begmuhommet.jumayev@gmail.com>
2025-01-04 08:15:05 +0800

    removed cache

--- src/mocks/mock-members.ts
+++
@@ -1,5 +0,0 @@
-export const mockMembers = [
-  { id: '1', name: 'Begmuhommet', telegram_username: '@begmuhommet' },
-  { id: '2', name: 'Unda', telegram_username: '@unda' },
-  { id: '3', name: 'Jamila', telegram_username: '@jamila' },
-];
